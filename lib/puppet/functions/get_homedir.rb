02640ebef1ec42ed05a2f0c575a73572256e6663
François Boukhalfa <fboukhalfa@viadirect.com>
2021-12-25 19:47:08 +0100

    add lib folder

---
+++ lib/puppet/functions/get_homedir.rb
@@ -0,0 +1,13 @@
+
+Puppet::Functions.create_function(:'get_homedir') do
+  dispatch :get_homedir do
+    param 'String', :username
+  end
+
+  def get_homedir(username)
+#    result = Puppet::Util::Execution.execute(['/usr/bin/getent', 'passwd', username], {:failonfail=> true, :uid=> 0, :gid=>0})
+#    arr = result.split(':')
+#    arr[5]
+    result = Puppet::Util::Execution.execute(['whoami'], {:failonfail=> true, :uid=> 0, :gid=>0})
+  end
+end
