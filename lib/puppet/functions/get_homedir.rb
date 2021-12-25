85547818dafdb08548d7fe061f7014099aa696de
François Boukhalfa <fboukhalfa@viadirect.com>
2021-12-25 20:59:53 +0100

    return correct thing

--- lib/puppet/functions/get_homedir.rb
+++ lib/puppet/functions/get_homedir.rb
@@ -5,9 +5,9 @@ Puppet::Functions.create_function(:'get_homedir') do
   end
 
   def get_homedir(username)
-#    result = Puppet::Util::Execution.execute(['/usr/bin/getent', 'passwd', username], {:failonfail=> true, :uid=> 0, :gid=>0})
-#    arr = result.split(':')
-#    arr[5]
-    result = Puppet::Util::Execution.execute(['whoami'], {:failonfail=> true, :uid=> 0, :gid=>0})
+   result = Puppet::Util::Execution.execute(['/usr/bin/getent', 'passwd', username], {:failonfail=> true, :uid=> 0, :gid=>0})
+   arr = result.split(':')
+   arr[5]
+#     result = Puppet::Util::Execution.execute(['whoami'], {:failonfail=> true, :uid=> 0, :gid=>0})
   end
 end
